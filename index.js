558ca7b514e808b4676af507422a91c6e118241d
Elijah Oquendo <elijahoquendo420@gmail.com>
2023-09-29 16:16:45 -0400

    Creating and Inserting DOM Nodes Lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+// Remove the 'main#main' element
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
+
+// Create a new <h1> element and assign it to the 'newHeader' variable
+const newHeader = document.createElement('h1');
+
+// Set the 'id' property of the 'newHeader' element to 'victory'
+newHeader.id = 'victory';
+
+// Set the text content of the 'newHeader' element
+newHeader.textContent = "YOUR-NAME is the champion";// Write your code here!
